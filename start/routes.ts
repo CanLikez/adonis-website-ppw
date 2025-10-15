5bf4d0eb64ba58ae8960dbc9d5bc035f540d9af1
CanLikez <candraafriansyah228@gmail.com>
2025-10-15 20:17:15 +0700

    imigrating hw1

--- start/routes.ts
+++ start/routes.ts
@@ -9,4 +9,7 @@
 
 import router from '@adonisjs/core/services/router'
 
-router.on('/').render('pages/home')
+router.on('/').render('pages/user/home')
+router.on('/product').render('pages/user/product')
+router.on('/about').render('pages/user/about')
+router.on('/checkout').render('pages/user/checkout')
